e3ce920ee34520d3eadbb8eeaf981f357ed03420
VenkatSairam <55329452+venkat2305@users.noreply.github.com>
2024-09-18 16:35:50 +0530

    fixed links in the footer component

--- components/Footer.js
+++ components/Footer.js
@@ -3,19 +3,19 @@ import React from "react"
 export default function Footer() {
     return (
         <div className="Footer" >
-            <a href="www.twitter.com"> 
+            <a href="https://x.com/home"> 
                 <img src="./images/Twitter-Icon.svg" alt="Twitter Icon" />
             </a>
-            <a href="www.facebook.com"> 
+            <a href="https://www.facebook.com/"> 
                 <img src="./images/Facebook-Icon.svg" alt="Twitter Icon" />
             </a>
-            <a href="www.instagram.com"> 
+            <a href="https://www.instagram.com/"> 
                 <img src="./images/Instagram-Icon.svg" alt="Twitter Icon" />
             </a>
-            <a href="www.linkedin.com"> 
+            <a href="https://www.linkedin.com/in/bhukya-venkat-sai-ram-320217193/"> 
                 <img src="./images/Linkedin-Icon.svg" alt="Twitter Icon" />
             </a>
-            <a href="https://www.github.com/"> 
+            <a href="https://github.com/venkat2305"> 
                 <img src="./images/GitHub-Icon.svg" alt="Twitter Icon" />
             </a>
         </div>
